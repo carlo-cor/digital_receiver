0ddad1abf5999be4307c567da8138488214e0112
Carlo Cortez <carlocorteez@gmail.com>
2025-11-28 17:15:32 -0800

    Initial commit; first draft of source.m and transmitter.m

---
+++ source.m
@@ -0,0 +1,24 @@
+% --------------------------------------------------
+% ---                 EENG 421                   ---
+% ---               Source Block                 ---
+% --------------------------------------------------
+function[message_encoded] = source()
+
+    global symbols;
+    global message;
+    
+    for dB = 1:11
+        if dB == 8
+            symbols = 2e+6;
+        elseif dB == 9
+            symbols = 5e+6;
+        elseif dB == 10
+            symbols = 10e+6;
+        else
+            symbols = 1e+6;
+        end
+        
+        message(dB) = symbols;
+        message_encoded = randi([0, 1], 1, symbols);
+    end
+end
